554dcab941c29abbdac50216d3bbac70282977c3
Andy Hunt <andyhunt353@gmail.com>
2020-05-29 10:57:52 +0100

    Update build

--- webpack.config.js
+++ webpack.config.js
@@ -19,11 +19,6 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'app/index.html'
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: 'app' }
-            ]
         })
     ],
     devServer: {
